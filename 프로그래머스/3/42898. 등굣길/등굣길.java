96166f3b78c64c62d71f0b8144465e42be4e2618
wwldwwld7 <133843645+wwldwwld7@users.noreply.github.com>
2023-11-19 23:46:08 +0900

    [level 3] Title: 등굣길, Time: 1.11 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -12,27 +12,15 @@ class Solution {
                 if(i-1>=0 && j-1>=0 && dp[i-1][j]==-1 && dp[i][j-1]==-1) dp[i][j] = -1;
                 else{
                     if(i-1 >= 0 && dp[i-1][j]!=-1){
-                        // if(dp[i][j] == 0) dp[i][j] = dp[i-1][j]+1;
-                        // else dp[i][j] = Math.min(dp[i][j], dp[i-1][j]+1);
                         dp[i][j] += dp[i-1][j];
                     }
                     if(j-1 >= 0 && dp[i][j-1]!=-1){
-                        // if(dp[i][j] == 0) {
-                        //     dp[i][j] = dp[i][j-1]+1;
-                        // }
-                        // else dp[i][j] = Math.min(dp[i][j], dp[i][j-1]+1);
                         dp[i][j] += dp[i][j-1];
                     }
                 }
                 dp[i][j] %= 1000000007;
             }
         }
-        // for(int i=0 ; i<n ; i++){
-        //     for(int j=0 ; j<m ; j++){
-        //         System.out.print(dp[i][j]);
-        //     }
-        //     System.out.println();
-        // }
         int answer = dp[n-1][m-1];
         return answer;
     }
